098c79c79ae8b07ebe3bdb6903f0ebd1a3384956
gmdfghi <91668752+gmdfghi@users.noreply.github.com>
2021-12-02 00:03:11 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
 
+#include"lib.h"
+
+bool numeroPrimo(int a){
+    bool primo = true;
+    for (int b=2; b>a; b++){
+        int risultato=a%b;
+        if (risultato==0 && b<a){
+            primo=false;
+            break;
+        }
+    }return primo;
+}
