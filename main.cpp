9ebedc014fe6f5ef74066250056277e0a803b953
gmdfghi <91668752+gmdfghi@users.noreply.github.com>
2021-12-02 00:02:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include "library.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    int numero;
+    cin>>numero;
+    if(numero==0){
+        cout<<"numero primo"<<endl;
+    }else if(numero<0){
+        return 0;
+    }else{
+        if (numeroPrimo(numero)==true){
+            cout<<"numero primo"<<endl;
+        }else{
+            cout <<"numero non primo"<<endl;
+        }
+    }
+    return 0;
 }
